91fa398ea740d1f2351a3b90bea1ae27ec7a267b
Sun He <sunheehnus@gmail.com>
2014-04-22 16:47:58 +0800

    122.c: done

--- 122.c
+++ 122.c
@@ -27,27 +27,102 @@ int num(char *a)
 	}
 	return n;
 }
+void reverse(int *array, int start, int end)
+{
+	int tmp;
+	while(start<end)
+	{
+		tmp = array[start];
+		array[start] = array[end];
+		array[end] = tmp;
+		start ++;
+		end --;
+	}
+}
+void shift(int *array, int length, int distance)
+{
+	reverse(array,0,length-distance-1);
+	reverse(array,length-distance,length-1);
+	reverse(array,0,length-1);
+}
 void DFS(int i)
 {
 	int j;
 	visit[i] = 1;
-	path[++total] = i;
-	if(total==N&&map[i][1])
-	{
-		finish = 1;
-		path[++total] = 1;
-		visit[i] = 0;
-		return;
-	}
+	path[total++] = i;
 	for(j=1;j<=N;j++)
 	{
-		if(!finish&&!visit[j]&&map[i][j])
+		if(!visit[j]&&map[i][j])
 		{
 			DFS(j);
+			break;
 		}
 	}
-	visit[i] = 0;
-	total--;
+}
+void Build()
+{
+	int i,j,z;
+
+	/* i) */
+	DFS(1);
+
+	/* ii) */
+	while(1)
+	{
+		/* a) */
+		if(map[path[0]][path[total-1]])
+		{
+			/* b) */
+			if(total==N)
+			{
+				finish = 1;
+				break;
+			}
+			/* c) */
+			else
+			{
+				for(z=1;z<=N;z++)
+				{
+					if(!visit[z])
+					{
+						for(i=1;i<=N;i++)
+						{
+							if(visit[i]&&map[z][i])
+								break;
+						}
+						if(i<=N)
+							break;
+					}
+				}
+				if(z<=N)
+				{
+					for(j=0;j<total;j++)
+					{
+						if(path[j]==i)
+							break;
+					}
+					shift(path,total,total-j);
+					for(i=total;i>0;i--)
+						path[i] = path[i-1];
+					path[0] = z;
+					visit[z] = 1;
+					total++;
+				}
+			}
+		}
+		/* iii) */
+		else
+		{
+			for(i=1;i<total-1;i++)
+			{
+				if(map[path[0]][path[i]]&&map[path[i-1]][path[total-1]])
+					break;
+			}
+			if(i>=total-1)
+				break;
+			reverse(path,i,total-1);
+		}
+	}
 }
 int main()
 {
@@ -67,20 +142,16 @@ int main()
 			map[i][j] = map[j][i] = 1;
 		}
 	}
-	/*for(i=1;i<=N;i++)*/
-	/*{*/
-		/*for(j=1;j<=N;j++)*/
-			/*printf("%d ",map[i][j]);*/
-		/*printf("\n");*/
-	/*}*/
-	DFS(1);
+	Build();
 	if(!finish)
 		printf("No solution\n");
 	else
 	{
-		for(i=1;i<=N;i++)
+		for(i=0;i<N&&path[i]!=1;i++);
+		shift(path,N,N-i);
+		for(i=0;i<N;i++)
 			printf("%d ",path[i]);
-		printf("%d\n",path[i]);
+		printf("%d\n",path[0]);
 	}
 	return 0;
 }
